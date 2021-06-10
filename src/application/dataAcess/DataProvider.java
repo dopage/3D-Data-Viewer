87e096f810fbb6d464d76c3771c2708352fc4e44
dopage <55894959+dopage@users.noreply.github.com>
2021-06-10 12:20:14 +0200

    Implémentation de la méthode getNbReportsByRegion()
    Permet de récupérer le nombre de signalements effectués dans chaque région et pour une espèce en particulier.

--- src/application/dataAcess/DataProvider.java
+++ src/application/dataAcess/DataProvider.java
@@ -31,14 +31,49 @@ public class DataProvider implements DataProviderInterface {
 	
 	@Override
 	public Species getNbReportsByRegion(String scientificName) {
-		// TODO Auto-generated method stub
-		return null;
+		return getNbReportsByRegion(scientificName, null, null);
 	}
-
+ 
 	@Override
 	public Species getNbReportsByRegion(String scientificName, Date from, Date to) {
-		// TODO Auto-generated method stub
-		return null;
+		Species species = new Species();
+		int minOccurence = 0;
+		int maxOccurence = 0;
+		String url = "https://api.obis.org/v3/occurrence/grid/3?";
+		if (scientificName != null) {
+			url += "scientificname=" + scientificName;
+		}
+		if (from != null && to != null) {
+			DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+			url += "&startdate=" + formatter.format(from) + "&enddate=" + formatter.format(to);
+		}
+		try {
+			JSONObject jsonRoot = new JSONObject(JSONHelper.readJsonFromUrl(url));
+			JSONArray listeDesRegions = jsonRoot.getJSONArray("features");
+			System.out.println("nb regions : " + listeDesRegions.length());
+			for (int i = 0; i < listeDesRegions.length(); i++) {
+				ArrayList<Point2D> points = new ArrayList<Point2D>();
+				JSONArray coords = listeDesRegions.getJSONObject(i).getJSONObject("geometry").getJSONArray("coordinates").getJSONArray(0);
+				for (int j = 0; j < coords.length(); j++) {
+					Point2D p = new Point2D(coords.getJSONArray(j).getDouble(0), coords.getJSONArray(j).getDouble(1));
+					points.add(p);
+				}
+				int nbReports = listeDesRegions.getJSONObject(i).getJSONObject("properties").getInt("n");
+				if (nbReports > maxOccurence)
+					maxOccurence = nbReports;
+				else if (nbReports < minOccurence)
+					minOccurence = nbReports;
+				Region region = new Region(points, nbReports);
+				species.addRegion(region);
+			}
+			species.setMinOccurrence(minOccurence);
+			species.setMaxOccurrence(maxOccurence);
+		}
+		catch (JSONException e) {
+			System.err.println("Erreur dans le json - getNbReportsByRegion()");
+			//e.printStackTrace();
+		}
+		return species;
 	}
 
 	@Override
--- src/application/dataAcess/DataProviderInterface.java
+++ src/application/dataAcess/DataProviderInterface.java
@@ -8,15 +8,27 @@ import javafx.geometry.Point2D;
 
 public interface DataProviderInterface {
 	
+	/**
+	 * Récupère le nombre de signalements effectués dans chaque région et pour une espèce en particulier
+	 * @param scientificName : le nom scientifique de l'espèce
+	 * @return : une instance de la classe "species" avec la liste des signalements présente dans son attribut "nbReportsByRegion"
+	 */
 	public Species getNbReportsByRegion(String scientificName);
 
+	/**
+	 * Récupère le nombre de signalements effectués dans chaque région et pour une espèce en particulier
+	 * @param scientificName : le nom scientifique de l'espèce
+	 * @param from : la date minimal des signalements
+	 * @param to : la date maximal des signalements
+	 * @return : une instance de la classe "species" avec la liste des signalements présente dans son attribut "nbReportsByRegion"
+	 */
 	public Species getNbReportsByRegion(String scientificName, Date from, Date to);
 
 	// retourne un tableau de taille nbIntervals contenant l'espèce et ses signalements par région pour chaque intervalle (exemple : l'élément 3 du tableau contient les signalements de l'espèce pour le 3ème pas de temps)
 	public ArrayList<Species> getNbReportsByZoneByTimeInterval(String scientificName, String geoHash, Date from, int timeInterval, int nbIntervals);
 	
 	/**
-	 * Récupére les premiers noms des espèces commençant par une chaîne de caractères passée en paramètre
+	 * Récupère les premiers noms des espèces commençant par une chaîne de caractères passée en paramètre
 	 * @param beginWith : le début du nom de l'espèce
 	 * @return : la liste des noms d'espèces correspondant
 	 */
