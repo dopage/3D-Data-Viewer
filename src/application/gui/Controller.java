b5db7173e275785dbbd156cc4d54da7c069809e0
Esteban <55922257+s-posito@users.noreply.github.com>
2021-06-13 10:50:33 +0200

    Interface Graphique
    Fin SceneBuilder + début Controller

--- src/application/gui/Controller.java
+++ src/application/gui/Controller.java
@@ -3,10 +3,54 @@ package application.gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.DatePicker;
 
 
 public class Controller implements Initializable {
+	
+	// Soyons Organisé
+	
+	// Tous les composants du PaneFind
+	@FXML
+	private Button btnSearch;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private DatePicker firstDate;
+	
+	@FXML
+	private DatePicker lastDate;
+	
+	
+	
+	@FXML
+	private Button btnPeriod;
+	
+	@FXML
+	private Button btnHisto;
+	
+	@FXML
+	private Button btnZoomMinus;
+	
+	@FXML
+	private Button btnZoomPlus;
+	
+	@FXML
+	private Button btnPlay;
+	
+	@FXML
+	private Button btnPause;
+	
+	@FXML
+	private Button btnStop;
+	
+	// 
 	
 	public Controller() {
 		
